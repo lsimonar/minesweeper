4c6664c5350e7a1888e209643d76f2abab26ed20
lsimonar <lluis.simon.92@gmail.com>
2022-12-01 12:35:35 +0100

    styles to different numbers

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -21,6 +21,32 @@ export function Cell({boardStatus, onClick, onRightClick}: CellProps) {
         return cellContent;
     }
 
+    const getCellStyle = (): string => {
+
+        let cellStyle : string = '';
+        if(boardStatus.neighbouringBombs !== 0){
+            switch(boardStatus.neighbouringBombs) {
+                case 1: 
+                    cellStyle = 'blue';
+                    break;
+                case 2: 
+                    cellStyle = 'green';
+                    break;
+                case 3:
+                    cellStyle = 'red';
+                    break;
+                case 4: 
+                    cellStyle = 'purple'
+                    break;
+                case 5: 
+                    cellStyle = 'maroon'
+                    break;
+                default: cellStyle = '';
+            }
+        }
+        return cellStyle;
+    }
+
     const click=()=>{
         onClick(boardStatus.index[0], boardStatus.index[1]); 
     }    
@@ -30,7 +56,7 @@ export function Cell({boardStatus, onClick, onRightClick}: CellProps) {
     }
 
     return (
-        <div onClick={click} onContextMenu={rightClick} className={boardStatus.revealed? 'cell revealed' : 'cell'}>
+        <div onClick={click} onContextMenu={rightClick} className={boardStatus.revealed? `cell revealed ${getCellStyle()}` : 'cell'}>
             {boardStatus.revealed? getCellContent() : boardStatus.flagged? '🚩' : ''}
         </div>
     )
